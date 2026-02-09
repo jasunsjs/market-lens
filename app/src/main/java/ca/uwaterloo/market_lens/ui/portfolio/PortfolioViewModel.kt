a7df0b7b38402b537aaacac810d474ef23fbf4b7
Blair Wang <wang.blair210@gmail.com>
2026-02-09 01:45:46 -0500

    create base portfolio page with nav

--- app/src/main/java/ca/uwaterloo/market_lens/ui/portfolio/PortfolioViewModel.kt
+++ app/src/main/java/ca/uwaterloo/market_lens/ui/portfolio/PortfolioViewModel.kt
@@ -1,4 +1,13 @@
 package ca.uwaterloo.market_lens.ui.portfolio
 
-class PortfolioViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class PortfolioViewModel : ViewModel() {
+    fun navigateToAlertPage(onSuccess: () -> Unit) {
+        viewModelScope.launch {
+            onSuccess()
+        }
+    }
 }
