b6e0bec06c73aa18ea5d4f889b3413ea7ad31211
Jason Sun <jasonsunjishen@gmail.com>
2026-03-03 22:08:40 -0500

    Add mock repositories to AppGraph and configure central domain model

--- app/src/main/java/ca/uwaterloo/market_lens/di/AppGraph.kt
+++ app/src/main/java/ca/uwaterloo/market_lens/di/AppGraph.kt
@@ -2,21 +2,26 @@ package ca.uwaterloo.market_lens.di
 
 import ca.uwaterloo.market_lens.data.mock.*
 import ca.uwaterloo.market_lens.domain.repository.*
-import ca.uwaterloo.market_lens.domain.service.MarketLensModel
+import ca.uwaterloo.market_lens.domain.service.*
 
 object AppGraph {
-//    val authRepository: AuthRepository = MockAuthRepository()
-//    val portfolioRepository: PortfolioRepository = MockPortfolioRepository()
+    val authRepository: AuthRepository = MockAuthRepository()
+    val portfolioRepository: PortfolioRepository = MockPortfolioRepository()
     val marketDataRepository: MarketDataRepository = MockMarketDataRepository()
-//    val eventsRepository: EventsRepository = MockEventsRepository()
-    // UNCOMMENT THESE WHEN IMPLEMENTED
+    val eventsRepository: EventsRepository = MockEventsRepository()
+    val alertsRepository: AlertsRepository = MockAlertsRepository()
+    val newsRepository: NewsRepository = MockNewsRepository()
+    val explanationRepository: ExplanationRepository = MockExplanationRepository()
+
 
     // Central domain model
-//    val model: MarketLensModel = MarketLensModelImpl(
-//        authRepository = authRepository,
-//        portfolioRepository = portfolioRepository,
-//        marketDataRepository = marketDataRepository,
-//        eventsRepository = eventsRepository,
-//
-//    )
+    val model: MarketLensModel = MarketLensModelImpl(
+        authRepository = authRepository,
+        portfolioRepository = portfolioRepository,
+        marketDataRepository = marketDataRepository,
+        eventsRepository = eventsRepository,
+        alertsRepository = alertsRepository,
+        newsRepository = newsRepository,
+        explanationRepository = explanationRepository,
+    )
 }
